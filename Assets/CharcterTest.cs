178309822daaf11de7fe06ac7a1223cfec6f4d8d
Hoz94 <141571478+Hoz94@users.noreply.github.com>
2023-12-22 15:32:04 +0900

    231222
    123

---
+++ Assets/CharcterTest.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharcterTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("ÃµÈ£Áø");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
