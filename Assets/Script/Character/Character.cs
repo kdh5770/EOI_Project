333a7e231012c37d77e49119cc3b01024fc80461
kdh5770 <127295079+kdh5770@users.noreply.github.com>
2024-01-04 12:37:54 +0900

    main

--- Assets/Script/Character/Character.cs
+++ Assets/Script/Character/Character.cs
@@ -23,23 +23,39 @@ public class Character : MonoBehaviour
         animator = GetComponentInChildren<Animator>();
         rigid = GetComponent<Rigidbody>();
         camera = Camera.main;
+
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (P_Hp <= 0)
-        {
-
-        }
+        //if (animator.GetBool("Aiming"))
+        //    transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
+        transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
     }
+
+    private void FixedUpdate()
+    {
+        moveDirection = (Vector3.right * input.x + Vector3.forward * input.y).normalized; // 기존엔 transform.right로 해서 플레이어의 회전값에 따라 계속 변하는데, 이를 Vector3.right 즉 World기준으로 이동하게 만들었다.
+
+        moveDirection = Quaternion.AngleAxis(camera.transform.rotation.eulerAngles.y, Vector3.up) * moveDirection; // 기존 _dir * y축 기준으로 카메라의 rotation.y값만큼 Quaternion을 리턴한다.
+
+        moveDirection = moveDirection.normalized; // 정규화
+
+        rigid.MovePosition(transform.position + (moveDirection * moveSpeed * Time.deltaTime)); // 이동
+
+
+    }
+
     private void LateUpdate()
     {
-        if (moveDirection != Vector3.zero) // _dir이 0이 아니라면, 즉! 움직이고 있다면,
-        {
-            Quaternion quat = Quaternion.LookRotation(moveDirection, Vector3.up); // 첫번째 인자는 바라보는 방향이며, 두번째 인자는 축이다. => 첫번째 인자는 바라보고자 하는 방향벡터가 들어가야한다.
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 360f * Time.deltaTime); // (첫번째) 에서 (두번째)까지 (세번째)의 속도로 회전한 결과를 리턴한다.
-        }
+
+        //if (moveDirection != Vector3.zero &&  !animator.GetBool("Aiming")) // _dir이 0이 아니라면, 즉! 움직이고 있다면,
+        //{
+        //    Quaternion quat = Quaternion.LookRotation(moveDirection, Vector3.up); // 첫번째 인자는 바라보는 방향이며, 두번째 인자는 축이다. => 첫번째 인자는 바라보고자 하는 방향벡터가 들어가야한다.
+        //    transform.rotation = Quaternion.RotateTowards(transform.rotation, quat, 360f * Time.deltaTime); // (첫번째) 에서 (두번째)까지 (세번째)의 속도로 회전한 결과를 리턴한다.
+        //}
 
         #region 기존 이동 로직
         //transform.rotation = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0);
@@ -96,16 +112,7 @@ public class Character : MonoBehaviour
         //rigid.MovePosition(transform.position + (moveDirection * moveSpeed * Time.deltaTime));
         #endregion
 
-        moveDirection = (Vector3.right * input.x + Vector3.forward * input.y).normalized; // 기존엔 transform.right로 해서 플레이어의 회전값에 따라 계속 변하는데, 이를 Vector3.right 즉 World기준으로 이동하게 만들었다.
 
-        moveDirection = Quaternion.AngleAxis(camera.transform.rotation.eulerAngles.y, Vector3.up) * moveDirection; // 기존 _dir * y축 기준으로 카메라의 rotation.y값만큼 Quaternion을 리턴한다.
-
-        moveDirection = moveDirection.normalized; // 정규화
-
-        rigid.MovePosition(transform.position + (moveDirection * moveSpeed * Time.deltaTime)); // 이동
-
-        if (animator.GetBool("Aiming"))
-            transform.rotation = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f);
     } // 이동
 
 
@@ -142,10 +149,10 @@ public class Character : MonoBehaviour
     {
         if (context.performed && Aiming)
         {
-            animator.SetBool("Shoot",true);
+            animator.SetBool("Shoot", true);
             Debug.Log("쏩니다.");
         }
-        else if(context.canceled)
+        else if (context.canceled)
         {
             animator.SetBool("Shoot", false);
         }
