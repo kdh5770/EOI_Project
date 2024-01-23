be1a84d4d74d98f377327e835e808cd7f4a1f25f
Hoz94 <141571478+Hoz94@users.noreply.github.com>
2024-01-23 17:48:37 +0900

    캐릭터
    타임라인 작업중

---
+++ Assets/Script/Character/CutScene.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+using UnityEngine.UI;
+
+public class CutScene : MonoBehaviour
+{
+    private PlayableDirector pd;
+    public TimelineAsset[] Tl;
+    [SerializeField]
+    private Text ShakeSceneText;
+    private CharacterInputSystem _input;
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pd=GetComponent<PlayableDirector>();
+        _input=GetComponent<CharacterInputSystem>();
+        _animator=GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnTriggerStay(Collider other) // 컷씬이 재생 될 박스 콜라이더에 머물면서 
+    {
+        if (other.gameObject.tag == "CutScene") 
+        {
+            if (_input.interaction) // 상호작용 키 e 를 눌렀을 때
+            {
+                StartCoroutine(ShakeDelayTime());
+                other.gameObject.SetActive(false);
+                pd.Play(Tl[0]);
+            }
+        }
+    }
+
+
+    IEnumerator ShakeDelayTime()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+}
