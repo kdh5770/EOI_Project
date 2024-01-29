3a18b112d822a3381526690339b7e458c1ca4351
Hoz94 <141571478+Hoz94@users.noreply.github.com>
2024-01-29 16:04:14 +0900

    유니크몬스터
    구현 시작

---
+++ Assets/Script/Monster/Unique_Monster/Mutant_Skill_A.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mutant_Skill_A : MonsterSkill
+{
+
+    public override void ApplyReaction(GameObject target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ApplySkillEffect(GameObject target)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void ExecuteAttack(GameObject _target)
+    {
+        throw new System.NotImplementedException();
+    }
+    public override void ActionAttack()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Script/Monster/Unique_Monster/Unique_FSM.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unique_FSM : MonsterFSM
+{
+
+    
+    public override void ChangeReactionState(REACT_TYPE _state)
+    {
+        throw new System.NotImplementedException();
+    }
+}
