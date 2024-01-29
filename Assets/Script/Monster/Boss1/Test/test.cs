fba271322dc3c12ab57afbe7d45acc893be8fcc9
Hoz94 <154575899+Hunasdasd@users.noreply.github.com>
2024-01-29 12:53:47 +0900

    몬스터 투사체

--- Assets/Script/Monster/Boss1/Test/test.cs
+++ Assets/Script/Monster/Boss1/Test/test.cs
@@ -6,7 +6,7 @@ public class test : MonoBehaviour
 {
     public List<Vector3> firstTargets;
     public Transform secondTarget;
-    public float waitTime = 3f; // 두 번째 목표지점으로 던지기 전 대기 시간
+    public float waitTime; // 두 번째 목표지점으로 던지기 전 대기 시간
 
     public GameObject bullet;
 
@@ -17,8 +17,10 @@ public class test : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        waitTime = 3f;
+        hasReachedFirstTarget = true;
 
-        for(int i =0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
             bullets.Add(Instantiate(bullet, transform.position, Quaternion.identity));
         }
@@ -33,11 +35,12 @@ public class test : MonoBehaviour
             waitTime -= Time.deltaTime;
             if (waitTime <= 0f)
             {
+                hasReachedFirstTarget = false;
                 foreach (GameObject bullet in bullets)
                 {
                     bullet.GetComponent<Rigidbody>().velocity = Vector3.zero;
                 }
-                //MoveToSecondTarget();
+                StartCoroutine(MoveToSecondTarget());
             }
         }
     }
@@ -45,23 +48,41 @@ public class test : MonoBehaviour
     void MoveToFirstTarget()
     {
 
-        for(int i = 0; i < 10; i++)
+        for (int i = 0; i < 10; i++)
         {
             Vector3 randomDirection = Random.onUnitSphere;
             Vector3 randomPosition = transform.up * 3 + randomDirection;
             firstTargets.Add(randomPosition);
         }
         // 첫 번째 목표지점으로 이동
-        for(int i = 0;i < 10;i++)
+        for (int i = 0; i < 10; i++)
         {
-            Vector3 dir = (firstTargets[i] - transform.position).normalized;
+            Vector3 dir = firstTargets[i].normalized;
             bullets[i].GetComponent<Rigidbody>().AddForce(dir * 5, ForceMode.Impulse);
         }
     }
 
-    public void MoveToSecondTarget()
+    IEnumerator MoveToSecondTarget()
     {
-        Vector3 directionToSecondTarget = (secondTarget.position - transform.position).normalized;
-        rb.AddForce(directionToSecondTarget * 10f, ForceMode.Impulse);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 25f);
+
+        if (colliders.Length > 0)
+        {
+            foreach (Collider col in colliders)
+            {
+                if (col.CompareTag("Player"))
+                {
+                    secondTarget = col.gameObject.transform;
+                    break;
+                }
+            }
+        }
+
+        foreach(GameObject bullet in bullets)
+        {
+            Vector3 directionToSecondTarget = (secondTarget.position - bullet.transform.position).normalized;
+            bullet.GetComponent<Rigidbody>().AddForce(directionToSecondTarget * 20, ForceMode.Impulse);
+            yield return new WaitForSeconds(.5f);
+        }
     }
 }
