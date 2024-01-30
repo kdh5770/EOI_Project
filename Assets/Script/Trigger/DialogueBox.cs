ebb5c08fecc767dd4d766b971aa1915cf5ff4e85
kdh5770 <127295079+kdh5770@users.noreply.github.com>
2024-01-30 13:04:19 +0900

    main
    인터렉션 문자열 입력창 크기 조절

--- Assets/Script/Trigger/DialogueBox.cs
+++ Assets/Script/Trigger/DialogueBox.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class DialogueBox : Interaction
 {
     [Header("UI에 출력할 대사를 적으세요")]
+    [TextArea(3, 10)]
     public string dialogueText;
 
     public override void Interact()
--- Assets/Script/Trigger/SystemMessage.cs
+++ Assets/Script/Trigger/SystemMessage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SystemMessage : Interaction
 {
     [Header("UI에 출력할 시스템 메세지를 적으세요")]
+    [TextArea(3, 10)]
     public string messageText;
     public override void Interact()
     {
