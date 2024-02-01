6703c7cc9de48aaced1c4eb610e159bacc7186cb
hoz <141571478+Hoz94@users.noreply.github.com>
2024-02-01 17:21:06 +0900

    캐릭터 UI 수정해야함
    bulletuimanager reference 오류 나서 추후 수정해야함

--- Assets/Script/Character/BulletUIManager.cs
+++ Assets/Script/Character/BulletUIManager.cs
@@ -2,18 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using Unity.VisualScripting;
+
 
 public class BulletUIManager : MonoBehaviour
 {
     [SerializeField]
-    private TextMeshPro BulletTxt;
-    CharacterStateController characterStateController;
+    private TMP_Text BulletTxt;
+    private CharacterStateController characterStateController;
 
 
     private void Start()
     {
-        BulletTxt=GetComponent<TextMeshPro>();
+ 
     }
 
     private void Update()
@@ -24,8 +24,15 @@ public class BulletUIManager : MonoBehaviour
 
     void HandleBulletUI()
     {
-        BulletTxt.text = characterStateController.curWeapon.Data.CurBullet.ToString()+"/"+characterStateController.curWeapon.Data.MaxBullet.ToString();
 
+        if (characterStateController != null)
+        {
+            BulletTxt.text = characterStateController.curWeapon.Data.CurBullet.ToString() + "/" + characterStateController.curWeapon.Data.MaxBullet.ToString();
+        }
+        else
+        {
+            Debug.Log("³Î");
+        }
     }
 
 }
