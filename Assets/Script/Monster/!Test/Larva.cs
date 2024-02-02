b0e8f7c9d7fb2ae151f60affb3a5419cf046bdb1
master <154575899+Hunasdasd@users.noreply.github.com>
2024-02-02 15:42:09 +0900

    장애물

--- Assets/Script/Monster/!Test/Larva.cs
+++ Assets/Script/Monster/!Test/Larva.cs
@@ -8,10 +8,21 @@ public class Larva : MonsterStatus
     public GameObject boomEft_;
     public Transform boom;
 
-    //IEnumerator boom()
-    //{
-    //    yield return ;
-    //}
+    public override void CalculateDamage(float _damage)
+    {
+        curHP -= _damage;
+        if (curHP <= 0)
+        {
+            LarvaBoom();
+        }
+    }
+
+    public void LarvaBoom()
+    {
+        boomEft_ = Instantiate(boomEft, boom.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(boomEft_, 2f);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
--- Assets/Script/Monster/!Test/burstEgg.cs
+++ Assets/Script/Monster/!Test/burstEgg.cs
@@ -2,10 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class burstEgg : MonoBehaviour
+public class burstEgg : MonsterStatus
 {
     public GameObject boomPre;
     public GameObject boomPre_;
+
+    public override void CalculateDamage(float _damage)
+    {
+        curHP -= _damage;
+        if (curHP <= 0)
+        {
+            EggBoom();
+        }
+    }
+
+    public void EggBoom()
+    {
+        boomPre_ = Instantiate(boomPre, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(boomPre_, 2f);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.CompareTag("Player"))
