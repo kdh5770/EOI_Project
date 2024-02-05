305d1238f19cbba595038e0aa1cf0be6229972b4
Hunasdasd <154575899+Hunasdasd@users.noreply.github.com>
2024-02-05 17:40:01 +0900

    몬스터

--- Assets/Script/Monster/!Test/burstEgg.cs
+++ Assets/Script/Monster/!Test/burstEgg.cs
@@ -21,13 +21,14 @@ public class burstEgg : MonsterStatus
     {
         gameObject.GetComponent<Collider>().enabled = false;
 
+        boomPreObj = Instantiate(boomEftPre, gameObject.transform.position, Quaternion.identity);
+        Destroy(gameObject);
+        Destroy(boomPreObj, 2f);
+
         if (target != null)
         {
             target.GetComponent<CharacterHealth>().TakeDamage(10);
         }
-        boomPreObj = Instantiate(boomEftPre, gameObject.transform.position, Quaternion.identity);
-        Destroy(gameObject);
-        Destroy(boomPreObj, 2f);
     }
 
     private void OnTriggerEnter(Collider other)
