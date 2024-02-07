abd35bce93f35ee2a28a5154f7b66ef454e6e7aa
master <154575899+Hunasdasd@users.noreply.github.com>
2024-02-07 13:46:03 +0900

    와쳐

--- Assets/Script/Monster/Watcher/Watcher_Larva.cs
+++ Assets/Script/Monster/Watcher/Watcher_Larva.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Watcher_Larva : MonsterSkill
 {
-    public GameObject watcher;
+    [Header("¹øµ¥±â")]
     public GameObject larva;
 
-    public override void ActionAttack()
-    {
-        throw new System.NotImplementedException();
-    }
+    NavMeshAgent Nav;
+    Rigidbody RB;
+
 
     public override void ApplyReaction(GameObject target)
     {
@@ -24,24 +24,51 @@ public class Watcher_Larva : MonsterSkill
 
     public override void ExecuteAttack(GameObject _target)
     {
+        Nav = GetComponent<NavMeshAgent>();
+        animationEvent.ActionAttack += ActionAttack;
+        animator.SetTrigger("isLarva");
+        Nav.enabled = false;
+    }
+
+    public override void ActionAttack()
+    {
         StartCoroutine(UP());
     }
 
     IEnumerator UP()
     {
-        Collider[] colliders = Physics.OverlapSphere(transform.position, 3f);
+        Collider[] colliders = Physics.OverlapSphere(transform.position, 1f);
 
         if (colliders.Length > 0)
         {
             foreach (Collider col in colliders)
             {
-                if (col.CompareTag("Larva"))
+                if (col.CompareTag("LarvaTf"))
                 {
-                    watcher.transform.position = col.transform.position;
+                    larva = col.gameObject;
                     break;
                 }
             }
         }
+
+        if (larva != null)
+        {
+            gameObject.transform.position = larva.transform.position;
+        }
+        else
+        {
+            animationEvent.ActionAttack -= ActionAttack;
+            animator.SetTrigger("isStopLoop");
+            
+        }
         yield return null;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("Wall"))
+        {
+
+        }
     }
 }
