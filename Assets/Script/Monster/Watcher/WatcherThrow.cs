45b6371a9a4c29158a6767d437eb446bb5ec167a
master <154575899+Hunasdasd@users.noreply.github.com>
2024-02-07 17:34:30 +0900

    몬스터
    와쳐 투사체 수정

--- Assets/Script/Monster/Watcher/WatcherThrow.cs
+++ Assets/Script/Monster/Watcher/WatcherThrow.cs
@@ -10,26 +10,31 @@ public class WatcherThrow : Attack
     public Transform BulletTf;
     [Header("생성된 발사체")]
     public GameObject BulletEft;
-    private const float offest = 1.5f;
+    private const float offest = 0.5f;
+    private Vector3 direction;
     public override void ExecuteAttack(GameObject _target)
     {
         animationEvent.ActionAttack += ActionAttack;
         target = _target;
         
 
-        Vector3 direction = ((target.transform.position + target.transform.up * offest) - transform.root.position).normalized;
+        direction = ((target.transform.position + target.transform.up * offest) - transform.root.position).normalized;
         transform.LookAt(target.transform.position);
-        BulletEft = Instantiate(BulletEftPre, BulletTf.transform.position, Quaternion.LookRotation(direction));
+
+        //BulletEft = Instantiate(BulletEftPre, BulletTf.transform.position, Quaternion.LookRotation(direction));
 
         //BulletEft.GetComponent<Rigidbody>().velocity = direction * 30f;
         //BulletEft.transform.LookAt(target.transform.position + target.transform.up * offest);
-      
+        
+        //BulletEft.GetComponent<Rigidbody>().AddForce(BulletEft.transform.forward * 15f, ForceMode.Impulse);
+        //Destroy(BulletEft, 5f);
 
         animator.SetTrigger("isThrow");
     }
 
     public override void ActionAttack()
     {
+        BulletEft = Instantiate(BulletEftPre, BulletTf.transform.position, Quaternion.LookRotation(direction));
         BulletEft.GetComponent<Rigidbody>().AddForce(BulletEft.transform.forward * 15f, ForceMode.Impulse);
         Destroy(BulletEft, 5f);
         animationEvent.ActionAttack -= ActionAttack;
