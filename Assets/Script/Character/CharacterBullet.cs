2e03906adebf006725d5d63d06cadf33f15e3933
kdh5770 <127295079+kdh5770@users.noreply.github.com>
2024-02-07 17:26:40 +0900

    main
    총알 버그 수정

--- Assets/Script/Character/CharacterBullet.cs
+++ Assets/Script/Character/CharacterBullet.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class CharacterBullet : MonoBehaviour
 {
+    private Rigidbody rigid;
     public float damage;
     public bool isCollision;
+
+    private void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
 
     public void SetBulle(float _damage)
     {
@@ -17,12 +24,21 @@ public class CharacterBullet : MonoBehaviour
         BullSetFalse();
     }
 
+    private void OnDisable()
+    {
+        rigid.velocity = Vector3.zero;
+    }
+
     void BullSetFalse()
     {
         RaycastHit hit;
         isCollision = Physics.Raycast(transform.position, transform.forward, out hit, 5,LayerMask.GetMask("Ground", "Monster"));
         if (isCollision)
         {
+            if (hit.collider.TryGetComponent(out Weakness weakness))
+            {
+                hit.collider.GetComponent<Weakness>().AttackDamage(damage, hit.transform.position);
+            }
             this.gameObject.SetActive(false);
         }
     }
