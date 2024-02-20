c69c26923706310955589d39c0bb9bf115afdb7d
aeme030 <154575899+Hunasdasd@users.noreply.github.com>
2024-02-20 13:40:14 +0900

    타임라인

--- Assets/Script/Trigger/CutSceneInteraction.cs
+++ Assets/Script/Trigger/CutSceneInteraction.cs
@@ -9,22 +9,24 @@ public class CutSceneInteraction : Interaction
     [Header("타임라인 에셋을 추가 하세요")]
     public TimelineAsset timeLine;
     public PlayableDirector pd;
-    public float asd;
+    [Header("타임라인 끝난 후 활성화되는 트리거")]
+    public GameObject bossCollider;
+    [Header("타임라인에 사용된 오브젝트 파괴")]
+    public List<GameObject> gameobjects;
 
     public override void Interact()
     {
-        //Time.timeScale = 0f; // 타임라인 실행 전에 모든 동작을 일시 정지
         Gamemanager.instance.timeLineManager.SetInteraction(pd, timeLine);
-        while(true)
-        {
-            asd += Time.deltaTime;
-        }
-        //pd.stopped += OnTimelineStopped; // 타임라인이 끝날 때 재개되도록 설정
+        pd.stopped += OnTimelineStopped; // 타임라인이 끝날 때 재개되도록 설정
     }
 
     private void OnTimelineStopped(PlayableDirector director) // 타임라인이 끝날 때 호출되는 메서드
     {
-        Time.timeScale = 1f; // 일시 정지를 해제하고 원래의 시간으로 복귀
+        foreach (GameObject interaction in gameobjects)
+        {
+            Destroy(interaction.gameObject);
+        }
+        bossCollider.SetActive(true);
         pd.stopped -= OnTimelineStopped; // 이벤트 리스너를 제거
     }
 }
