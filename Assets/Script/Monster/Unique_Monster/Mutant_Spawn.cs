960c5aff8d45d46eb61593d2bbb1443fc5068004
aeme030 <154575899+Hunasdasd@users.noreply.github.com>
2024-02-27 13:43:30 +0900

    뮤턴트 소환 완료

--- Assets/Script/Monster/Unique_Monster/Mutant_Spawn.cs
+++ Assets/Script/Monster/Unique_Monster/Mutant_Spawn.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Mutant_Spawn : MonoBehaviour
 {
-    [Header("몬스터 유무")]
+    [Header("몬스터&플레이어 유무")]
     public Transform monster;
+    public Transform player;
     [Header("보스&이펙트 소환 위치")]
     public Transform spawn_Boss;
     [Header("뮤턴트 프리팹")]
@@ -16,8 +17,11 @@ public class Mutant_Spawn : MonoBehaviour
     public void Update()
     {
         int monsterLayer = LayerMask.NameToLayer("Monster");
+        string playerTag = "Player";
+
         Collider[] colliders = Physics.OverlapSphere(transform.position, 20f);
         monster = null;
+        player = null;
 
         if (colliders.Length > 0)
         {
@@ -28,10 +32,14 @@ public class Mutant_Spawn : MonoBehaviour
                     monster = col.gameObject.transform;
                     break;
                 }
+                else if (col.gameObject.CompareTag(playerTag))
+                {
+                    player = col.gameObject.transform;
+                }
             }
         }
 
-        if (monster == null)
+        if (monster == null && player != null)
         {
             GameObject Mutant = Instantiate(boss, spawn_Boss.transform.position, Quaternion.identity);
             GameObject Eft = Instantiate(eft, spawn_Boss.transform.position, Quaternion.identity);
