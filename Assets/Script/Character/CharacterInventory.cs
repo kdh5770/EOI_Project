d31bfe64671f8a26c8e9cb3be817be33e59f305a
master <141571478+Hoz94@users.noreply.github.com>
2024-03-04 17:53:12 +0900

    .
    .

--- Assets/Script/Character/CharacterInventory.cs
+++ Assets/Script/Character/CharacterInventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharcterInventory : MonoBehaviour
+public class CharacterInventory : MonoBehaviour
 {
     bool isget;
 
