be214fc874e9208c50ed76055a0be969bc376f45
master <141571478+Hoz94@users.noreply.github.com>
2024-03-04 16:28:50 +0900

    .
    .

--- Assets/Script/Character/CharcterInventory.cs
+++ Assets/Script/Character/CharcterInventory.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class CharcterInventory : MonoBehaviour
 {
     [SerializeField]
     private List<GameObject> InvenObj = new List<GameObject>();
