6da095e398df494fa25fc3162fc40ee441aa4dac
Hoz94 <141571478+Hoz94@users.noreply.github.com>
2024-07-25 15:54:59 +0900

    Update GunSoundEvent.cs

--- Assets/Script/Character/GunSoundEvent.cs
+++ Assets/Script/Character/GunSoundEvent.cs
@@ -16,13 +16,8 @@ public class GunSoundEvent : MonoBehaviour
     public void gunSoundEvent()
     {
         //ActionAttack?.Invoke();
-        if (gunsound != null && gunsound.clip != null)
-        {
-            gunsound.Play();
-            machine.Data.CurBullet--;
-        }
-/*        gunsound.Play();
-        machine.Data.CurBullet--;*/
+        gunsound.Play();
+        machine.Data.CurBullet--;
     }
 
     public void EndAnimation() // 공격 애니메이션 종료시 호출할 함수
